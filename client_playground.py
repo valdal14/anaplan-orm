1bd76635884c54954dddbe27660dcbde48d98227
Valerio DAlessio <u105550@canda.com>
2026-03-09 08:29:54 +0100

    refactored the client_playground

--- client_playground.py
+++ client_playground.py
@@ -1,20 +1,24 @@
-from anaplan_orm.client import AnaplanClient, Authenticator
+from anaplan_orm.client import AnaplanClient
 from anaplan_orm.client import BasicAuthenticator
 from anaplan_orm.exceptions import AnaplanConnectionError
 
-# Basic Auth Playground
-print("--> Initializing AnaplanClient...")
-basic_auth = BasicAuthenticator(
-    email="xxxxxx@xxxxx.com", # change with your Anaplan's account username
-    pwd="xxxxxxx", # change with your Anaplan's account password
-    verify_ssl=False
-)
+def ping_anaplan():
+    # Basic Auth Playground
+    print("--> Initializing AnaplanClient...")
+    basic_auth = BasicAuthenticator(
+        email="win-anaplan@canda.com", # change with your Anaplan's account username
+        pwd="ApTb2343", # change with your Anaplan's account password
+        verify_ssl=False
+    )
 
-client = AnaplanClient(authenticator=basic_auth, verify_ssl=False)
-print("--> Pinging Anaplan API...")
+    client = AnaplanClient(authenticator=basic_auth, verify_ssl=False)
+    print("--> Pinging Anaplan API...")
 
-try:
-    status_code = client.ping()
-    print(f"\n✅ Result: The Anaplan API responded with HTTP Status: {status_code}")
-except AnaplanConnectionError as e:
-    print(f"\n❌ Pipeline Failed: {e}")
+    try:
+        status_code = client.ping()
+        print(f"\n✅ Result: The Anaplan API responded with HTTP Status: {status_code}")
+    except AnaplanConnectionError as e:
+        print(f"\n❌ Pipeline Failed: {e}")
+
+# execute the ping 
+ping_anaplan()
